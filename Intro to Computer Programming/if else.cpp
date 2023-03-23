c1771f6f1f72aef9675532892270be4ab7be7e6a
Nofal Zia <128501008+Nofalzia@users.noreply.github.com>
2023-03-23 13:22:10 +0500

    Create if else.cpp

---
+++ Intro to Computer Programming/if else.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout << "Enter a Number ";
+	cin >> a;
+
+	if (a % 2 == 0)
+		cout << a << " is an Even number \n";
+	else
+		cout << a << " is an Odd number \n";
+
+}
